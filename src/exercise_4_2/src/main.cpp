7c4f67b270f886b02d7e1ba716ad30e0a55267b8
Damiano Zanardo <damiano.zanardo@gmail.com>
2022-11-25 13:15:07 +0000

    exercise_4_2 complete

--- src/exercise_4_2/src/main.cpp
+++ src/exercise_4_2/src/main.cpp
@@ -2,8 +2,6 @@
 #include <ros/ros.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <tf2_ros/transform_broadcaster.h>
-#include <tf2_ros/transform_listener.h>
 
 #include <opencv2/calib3d.hpp>
 #include <opencv2/core.hpp>
@@ -12,16 +10,13 @@
 
 class Boh {
  private:
-  tf2_ros::Buffer buffer;
-  tf2_ros::TransformListener* listener;
-
   ros::NodeHandle node;
   ros::Subscriber tag_subscriber;
 
   std::vector<geometry_msgs::PoseWithCovarianceStamped> points;
 
-  cv::Point3d meanPoint;
-  cv::Point2d meanPoint2D;
+  cv::Point3f meanPoint;
+  cv::Point2f meanPoint2D;
 
   std::string saveImagePath;
   cv::Mat image;
@@ -34,16 +29,8 @@ class Boh {
 
     for (size_t i = 0; i < msg->detections.size(); i++) {
       geometry_msgs::PoseWithCovarianceStamped original(msg->detections[i].pose);
-      // geometry_msgs::PoseWithCovarianceStamped transformed = buffer.transform(original, "base_link", ros::Time(0), original.header.frame_id);
-      // transformed.header.seq = original.header.seq;
-
-      // points.push_back(transformed);
 
       if (points.size() == 0 || original.header.seq != points.at(0).header.seq) {
-        // geometry_msgs::PoseWithCovarianceStamped transformed = buffer.transform(original, "base_link", ros::Time(0), original.header.frame_id);
-
-        // transformed.header.seq = original.header.seq;
-
         points.push_back(original);
       } else {
         ROS_INFO("shutdown");
@@ -60,27 +47,26 @@ class Boh {
   void loadCameraInfo() {
     boost::shared_ptr<const sensor_msgs::CameraInfo> cameraInfo = ros::topic::waitForMessage<sensor_msgs::CameraInfo>("kinect/rgb/camera_info", ros::Duration(5));
 
-    float tempK[9];
-    for (size_t i = 0; i < 9; i++) {
-      tempK[i] = cameraInfo->K[i];
+    std::vector<float> tempK;
+    for (size_t i = 0; i < cameraInfo->K.size(); i++) {
+      tempK.push_back(cameraInfo->K[i]);
     }
+    cameraMatrix = cv::Mat(3, 3, CV_32F, tempK.data()).clone();
 
-    cameraMatrix = cv::Mat(3, 3, CV_32F, tempK);
-
-    distCoeffs = cv::Mat(cameraInfo->D);
-
-    std::cout << cameraMatrix << std::endl;
-    std::cout << distCoeffs << std::endl;
+    std::vector<float> tempD;
+    for (size_t i = 0; i < cameraInfo->D.size(); i++) {
+      tempD.push_back(cameraInfo->D[i]);
+    }
+    distCoeffs = cv::Mat(1, 8, CV_32F, tempD.data()).clone();
   }
 
   void processPoints() {
+    ROS_INFO("There are %d points", points.size());
     for (size_t i = 0; i < points.size(); i++) {
-      std::cout << points[i].pose.pose.position.x << " " << points[i].pose.pose.position.y << " " << points[i].pose.pose.position.z << std::endl;
       meanPoint.x += points[i].pose.pose.position.x;
       meanPoint.y += points[i].pose.pose.position.y;
       meanPoint.z += points[i].pose.pose.position.z;
     }
-    std::cout << meanPoint << std::endl;
     meanPoint = meanPoint / (float)points.size();
 
     ROS_INFO("The mean point is (%f, %f, %f)", meanPoint.x, meanPoint.y, meanPoint.z);
@@ -89,16 +75,14 @@ class Boh {
   void projectMean() {
     cv::Mat rvec = (cv::Mat_<float>(3, 1) << 0.0, 0.0, 0.0);
     cv::Mat tvec = (cv::Mat_<float>(3, 1) << meanPoint.x, meanPoint.y, meanPoint.z);
-    // cv::Mat tvec(meanPoint);
 
-    std::vector<cv::Point3d> tempPoints({meanPoint});
-    std::vector<cv::Point2d> tempPoints2D;
+    std::vector<cv::Point3f> tempPoints;
+    tempPoints.push_back(meanPoint);
+    std::vector<cv::Point2f> tempPoints2D;
 
     cv::projectPoints(tempPoints, rvec, tvec, cameraMatrix, distCoeffs, tempPoints2D);
 
     meanPoint2D = tempPoints2D[0];
-
-    ROS_INFO("The mean point in 2D is (%f, %f)", meanPoint2D.x, meanPoint2D.y);
   }
 
   void drawMean() {
@@ -111,7 +95,6 @@ class Boh {
     image = cv::imread(imagePath);
     saveImagePath = imagePath + ".saved.png";
 
-    listener = new tf2_ros::TransformListener(buffer, node);
     tag_subscriber = node.subscribe("tag_detections", 1000, &Boh::callback, this);
 
     ROS_INFO("Init");
